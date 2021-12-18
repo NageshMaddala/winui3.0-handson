6fb9225ae1d1fb2013c04f20362c7f8cf379adc9
Nagesh_Maddala <nagesh_maddala@dell.com>
2021-12-17 20:05:12 -0600

    with basic viewmodel

---
+++ Winui3POC/TestApp01/ViewModels/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Input;
+using Microsoft.UI.Xaml.Input;
+namespace TestApp01.ViewModels;
+using System;
+
+public class RelayCommand : ICommand
+{
+    private readonly Action action;
+    private readonly Func<bool> canExecute;
+
+    public RelayCommand(Action action)
+        : this(action, null)
+    {
+    }
+
+    public RelayCommand(Action action, Func<bool> canExecute)
+    {
+        if (action == null)
+            throw new ArgumentNullException(nameof(action));
+
+        this.action = action;
+        this.canExecute = canExecute;
+    }
+
+    event EventHandler ICommand.CanExecuteChanged
+    {
+        add
+        {
+        }
+
+        remove
+        {
+        }
+    }
+
+    public bool CanExecute(object parameter) => canExecute == null || canExecute();
+
+    public void Execute(object parameter) => action();
+
+    public event EventHandler<object> CanExecuteChanged;
+
+    public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+}
--- Winui3POC/TestApp01/Views/TestPage1.xaml.cs
+++ Winui3POC/TestApp01/Views/TestPage1.xaml.cs
@@ -15,6 +15,7 @@ using Windows.Foundation.Collections;
 using Windows.UI.Popups;
 using TestApp01.Model;
 using TestApp01.Enums;
+using TestApp01.ViewModels;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -34,98 +35,8 @@ namespace TestApp01.Views
         public TestPage1()
         {
             InitializeComponent();
-            Loaded += TestPage1_Loaded;
-            ItemList.Loaded += ItemList_Loaded;
-            ItemFilter.Loaded += ItemFilter_Loaded;
         }
 
-        private void ItemFilter_Loaded(object sender, RoutedEventArgs e)
-        {
-            var filterCombo = (ComboBox)sender;
-            PopulateData();
-            filterCombo.ItemsSource = _mediums;
-            filterCombo.SelectedIndex = 0;
-        }
-
-        private void TestPage1_Loaded(object sender, RoutedEventArgs e)
-        {
-            ItemFilter.SelectionChanged += ItemFilter_SelectionChanged;
-            PopulateData();
-        }
-
-        private void ItemFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var updatedItems = (from item in _allItems
-                where string.IsNullOrWhiteSpace(ItemFilter.SelectedValue.ToString()) ||
-                      ItemFilter.SelectedValue.ToString() == "All" ||
-                      ItemFilter.SelectedValue.ToString() == item.MediaType.ToString()
-                select item).ToList();
-            ItemList.ItemsSource = updatedItems;
-        }
-
-        private void ItemList_Loaded(object sender, RoutedEventArgs e)
-        {
-            var listView = (ListView)sender;
-            PopulateData();
-            listView.ItemsSource = _items;
-        }
-
-        private async void AddButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
-        {
-            var dialog = new MessageDialog("Adding items to the collection is not yet available.", "My Media Collection");
-            await dialog.ShowAsync();
-        }
-
-        public void PopulateData()
-        {
-            if (_isLoaded) return;
-            _isLoaded = true;
-
-            var cd = new MediaItem
-            {
-                Id = 1,
-                Name = "Classical Favorites",
-                MediaType = ItemType.Music,
-                MediumInfo = new Medium { Id = 1, MediaType = ItemType.Music, Name = "CD" }
-            };
-
-            var book = new MediaItem
-            {
-                Id = 2,
-                Name = "Classic Fairy Tales",
-                MediaType = ItemType.Book,
-                MediumInfo = new Medium { Id = 2, MediaType = ItemType.Book, Name = "Book" }
-            };
-
-            var bluRay = new MediaItem
-            {
-                Id = 3,
-                Name = "The Mummy",
-                MediaType = ItemType.Video,
-                MediumInfo = new Medium { Id = 3, MediaType = ItemType.Video, Name = "Blu Ray" }
-            };
-
-            _items = new List<MediaItem>
-            {
-                cd,
-                book,
-                bluRay
-            };
-
-            _allItems = new List<MediaItem>
-            {
-                cd,
-                book,
-                bluRay
-            };
-
-            _mediums = new List<string>
-            {
-                "All",
-                nameof(ItemType.Book),
-                nameof(ItemType.Music),
-                nameof(ItemType.Video)
-            };
-        }
+        public MainViewModel ViewModel => new MainViewModel();
     }
 }
